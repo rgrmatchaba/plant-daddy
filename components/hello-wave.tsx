587214415b19f0b065e7706a9f73ca711b53da38
Rue Matchaba <rgrmatchaba@gmail.com>
2025-11-07 14:57:58 +0200

    seond commit

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -10,7 +10,7 @@ export function HelloWave() {
         animationName: {
           '50%': { transform: [{ rotate: '25deg' }] },
         },
-        animationIterationCount: 4,
+        animationIterationCount: 5,
         animationDuration: '300ms',
       }}>
       👋
